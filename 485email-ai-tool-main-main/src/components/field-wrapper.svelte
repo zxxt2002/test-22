5ac8bd24df8df6a5a9cf9e79752e9f3aab790b80
xiantong zhao <99571021+zxxt2002@users.noreply.github.com>
2023-11-19 14:30:16 -0700

    Delete 485email-ai-tool-main-main directory

--- 485email-ai-tool-main-main/src/components/field-wrapper.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    export let label =""
-    export let id=""
-</script>
-
-<div class="flex flex-col mb-6">
-    <label class="mb-2 text-md" for={id}>{label}</label>
-    <slot />
-</div>
--- 485email-ai-tool-main-main/src/routes/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import '../app.css'
-	import Header from '../components/header.svelte';
-</script>
-
-<div class="bg-primary text-white min-h-screen">
-	<Header />
-	<slot />
-</div>
